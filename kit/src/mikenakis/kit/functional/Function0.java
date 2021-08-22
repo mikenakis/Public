fb44714bde5a002589863bb89c7a43c94989d47b
mikenakis <git@michael.gr>
2021-08-22 18:36:54 +0200

    refactoring the spooling

--- kit/src/mikenakis/kit/functional/Function0.java
+++ kit/src/mikenakis/kit/functional/Function0.java
@@ -1,5 +1,7 @@
 package mikenakis.kit.functional;
 
+import java.util.function.Function;
+
 /**
  * A method which accepts no arguments and returns a value.
  * (Corresponds to Java's {@link java.util.function.Supplier}.)
@@ -10,5 +12,10 @@ package mikenakis.kit.functional;
  */
 public interface Function0<R>
 {
+	static <R> R invoke( Function0<R> function )
+	{
+		return function.invoke();
+	}
+
 	R invoke();
 }
